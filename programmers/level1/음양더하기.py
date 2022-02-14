5f8db08cca7b0c7f51c53a6a437d9e9032f529f6
Booooms <1222kbj@naver.com>
2022-02-14 15:51:31 +0900

    add PS

---
+++ programmers/level1/음양더하기.py
@@ -0,0 +1,12 @@
+def solution(absolutes, signs):
+    result = 0
+    for i in range(len(signs)):
+        if signs[i]:
+            result += absolutes[i]
+        else:
+            result -= absolutes[i]
+    return result
+
+# other solution
+# def solution(absolutes, signs):
+#     return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
