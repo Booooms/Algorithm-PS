19152752fbc5f6fb8f3c7e4198515b365a2907c3
Booooms <1222kbj@naver.com>
2022-03-11 14:41:52 +0900

    add PS

---
+++ programmers/level2/영어끝말잇기.py
@@ -0,0 +1,23 @@
+def solution(n, words):
+    used_words = []
+    number, order = 0,0
+
+    used_words.append(words[0])
+    last_word = words[0][-1]
+    
+    for i in range(1,len(words)):
+        if words[i] not in used_words and words[i][0] == last_word:
+            used_words.append(words[i])
+            last_word = words[i][-1]
+        else:
+            number = (i%n)+1
+            order = (i//n)+1
+            break
+    return [number, order]
+
+# other solution 
+# def solution(n, words):
+#     for p in range(1, len(words)):
+#         if words[p][0] != words[p-1][-1] or words[p] in words[:p]: return [(p%n)+1, (p//n)+1]
+#     else:
+#         return [0,0]
