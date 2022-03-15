e0640e1a15347e0a760d5c13fb91275149c0284a
Booooms <1222kbj@naver.com>
2022-03-15 14:42:26 +0900

    add PS

--- programmers/level2/튜플.py
+++ programmers/level2/튜플.py
@@ -1,13 +1,16 @@
 def solution(s):
     answer = []
-
-    tuple_set = s[2:-2].split('},{')
-    tuple_set.sort(key=len)
-
-    for i, t in enumerate(tuple_set):
-        t = list(map(int, t.split(",")))
-        for ch in t:
-            if ch not in answer:
-                answer.append(ch)
-            
+    sp_s = (s[2:-2].split('},{'))
+    sp_s.sort(key=len)
+    for i in sp_s:
+        list_s = list(map(int, i.split(',')))
+        for j in list_s:
+            if j not in answer:
+                answer.append(j)
     return answer
+
+# other solution
+# from collections import Counter
+# def solution(s):
+#     new_s = [sss.replace('{','').replace('}','') for sss in s.split(',')]
+#     return [int(c[0]) for c in sorted(Counter(new_s).items(), key = lambda x: x[1],reverse=True )]
