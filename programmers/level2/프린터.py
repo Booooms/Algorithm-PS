634088c6061d3b217d100ab9641bf0128f5ec51b
Booooms <1222kbj@naver.com>
2022-04-03 16:08:48 +0900

    add PS

--- programmers/level2/프린터.py
+++ programmers/level2/프린터.py
@@ -1,23 +1,32 @@
 def solution(priorities, location):
     answer = 0
-    # num, priority
-    queue = []
-    for i, p in enumerate(priorities):
-        queue.append((i,p))
-
+    queue = [(i, p) for i, p in enumerate(priorities)]
     while queue:
         flag = 0
-        num, priority = queue.pop(0)
-        
+        check = queue.pop(0)
+
         for x, y in queue:
-            if priority < y:
+            if check[1] < y:
                 flag = 1
 
         if flag == 0:
             answer+=1
-            if location == num:
+            if location == check[0]:
                 break
         else:
-            queue.append((num, priority))         
+            queue.append((check[0], check[1]))         
 
     return answer
+
+# other solution
+# def solution(priorities, location):
+#     queue =  [(i,p) for i,p in enumerate(priorities)]
+#     answer = 0
+#     while True:
+#         cur = queue.pop(0)
+#         if any(cur[1] < q[1] for q in queue):
+#             queue.append(cur)
+#         else:
+#             answer += 1
+#             if cur[0] == location:
+#                 return answer
