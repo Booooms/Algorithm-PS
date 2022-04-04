f64570f286850542e292c5fcb2719282242d23cf
Booooms <1222kbj@naver.com>
2022-04-04 12:16:37 +0900

    add PS

--- programmers/level2/더맵게.py
+++ programmers/level2/더맵게.py
@@ -1,18 +1,35 @@
 import heapq
 
-# heapq is faster than sort when using min, max value(iteration)
-
 def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
 
-    while scoville[0]<K:
-        first_min_value = heapq.heappop(scoville)
-        second_min_value = heapq.heappop(scoville)
-        heapq.heappush(scoville, first_min_value+2*second_min_value)
-        answer+=1
+    while scoville[0] < K:
+        first_min = heapq.heappop(scoville)
+        second_min = heapq.heappop(scoville)
+        heapq.heappush(scoville, first_min + second_min*2)
+        answer +=1
 
-        if len(scoville)==1 and scoville[0]<K:
+        if len(scoville) == 1 and scoville[0] < K:
             return -1
-    
+
     return answer
+
+# other solution
+# import heapq as hq
+
+# def solution(scoville, K):
+
+#     hq.heapify(scoville)
+#     answer = 0
+#     while True:
+#         first = hq.heappop(scoville)
+#         if first >= K:
+#             break
+#         if len(scoville) == 0:
+#             return -1
+#         second = hq.heappop(scoville)
+#         hq.heappush(scoville, first + second*2)
+#         answer += 1  
+
+#     return answer
