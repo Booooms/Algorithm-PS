6ce3a88cf526684bd83259c579bed1383df8bf9d
Booooms <1222kbj@naver.com>
2022-04-04 17:40:58 +0900

    add PS

--- programmers/level2/소수찾기.py
+++ programmers/level2/소수찾기.py
@@ -1,30 +1,24 @@
-from itertools import permutations
-
-def isPrime(number):
-    cnt=0
-
-    if number in [0,1]:
+import itertools as it
+def is_prime(n):
+    if n in [0,1]:
         return False
-
-    for i in range(1,int(number**0.5)+1):
-        if number % i ==0:
-            cnt+=1
     
-    return True if cnt==1 else False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
 
 def solution(numbers):
-    answer = 0
+    answer = []
     nums = list(numbers)
-    p = []
-    permutation = []
-
+    tmp = []
+    
     for i in range(1, len(nums)+1):
-        permutation.extend(list(permutations(nums, i)))
+        tmp += (list(it.permutations(nums, i)))
+    hap = [int(''.join(t)) for t in tmp]
 
-    p = [int(''.join(x)) for x in permutation]
+    for j in hap:
+        if is_prime(j):
+            answer.append(j)
 
-    for i in set(p):
-        if isPrime(i):
-            answer += 1
-
-    return answer
+    return len(set(answer))
