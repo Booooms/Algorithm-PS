619bf20db9b5dd01738ec0029538534fb9ea81ea
Booooms <1222kbj@naver.com>
2022-04-05 17:27:21 +0900

    add PS

--- programmers/level2/조이스틱.py
+++ programmers/level2/조이스틱.py
@@ -1,14 +1,16 @@
 def solution(name):
     answer = 0
-    min_left_right = len(name) 
-    next_idx = 0
+    min_move = len(name) - 1
+    
     for idx, char in enumerate(name):
+        # 알파벳 변경 최솟값
         answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        
         next_idx = idx + 1
         while next_idx < len(name) and name[next_idx] == 'A':
             next_idx += 1 
         
         
-        min_left_right = min(min_left_right, idx + idx + len(name) - next_idx)
-    answer += min_left_right
+        min_move = min([min_move, idx*2 + len(name) - next_idx, idx + 2*(len(name) -next_idx)])
+    answer += min_move
     return answer
