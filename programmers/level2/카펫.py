34c9694bddd638f70631f818938a29c3500e206f
Booooms <1222kbj@naver.com>
2022-04-05 15:22:40 +0900

    add PS

--- programmers/level2/카펫.py
+++ programmers/level2/카펫.py
@@ -9,3 +9,10 @@ def solution(brown, yellow):
                     return [a,b]
             
     return answer
+
+# other solution
+# def solution(brown, yellow):
+#     for i in range(1, int(yellow**(1/2))+1):
+#         if yellow % i == 0:
+#             if 2*(i + yellow//i) == brown-4: 꼭짓점개수 빼기(겹치는부분)
+#                 return [yellow//i+2, i+2]
