9a1715588019977cd67b85f7a13b6b816c11c3da
Booooms <1222kbj@naver.com>
2022-04-07 21:14:07 +0900

    add PS

--- programmers/level2/타겟넘버.py
+++ programmers/level2/타겟넘버.py
@@ -12,3 +12,19 @@ def solution(numbers, target):
             if temp == target:
                 answer += 1
     return answer
+
+# other solution
+# def solution(numbers, target):
+#     if not numbers and target == 0 :
+#         return 1
+#     elif not numbers:
+#         return 0
+#     else:
+#         return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+
+# other solution
+# from itertools import product
+# def solution(numbers, target):
+#     l = [(x, -x) for x in numbers]
+#     s = list(map(sum, product(*l)))
+#     return s.count(target)
