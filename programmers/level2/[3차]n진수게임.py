9b29dea5bf4d2cb42c1536e17fb1699ba3c59f1d
Booooms <1222kbj@naver.com>
2022-04-13 17:42:32 +0900

    add PS

---
+++ programmers/level2/[3차]n진수게임.py
@@ -0,0 +1,25 @@
+def solution(n, t, m, p):
+
+    #재귀함수 10진수 -> n진수
+    def convert(n, base):
+        arr = "0123456789ABCDEF"
+        q, r = divmod(n, base)
+        if q == 0:
+            return arr[r]
+        else:
+            return convert(q, base) + arr[r]
+
+    answer = ''
+    candidate = []
+
+ 
+    for i in range(t*m):
+        conv = convert(i, n)
+        for c in conv:
+            candidate.append(c)
+
+    
+    for i in range(p-1, t*m, m):
+        answer += candidate[i]
+
+    return answer
