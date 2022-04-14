892431050e4552794327d2dc439a53ec87350c60
Booooms <1222kbj@naver.com>
2022-04-14 20:47:07 +0900

    add PS

---
+++ programmers/level2/[3차]방금그곡.py
@@ -0,0 +1,28 @@
+import math
+def solution(m, musicinfos):
+    
+    m = m.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a")
+    answer = None
+    for musicinfo in musicinfos:
+        start, end, title, code = musicinfo.split(",")
+        hour, minute = map(int, start.split(":"))
+        start = hour * 60 + minute
+
+        hour, minute = map(int, end.split(":"))
+        end = hour * 60 + minute
+        duration = end - start
+
+        code = code.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a")
+        code *= math.ceil(duration / len(code))
+        code = code[:duration]
+        
+        if m not in code:
+            continue
+
+        if answer == None or answer[0] < duration or (answer[0] == duration and answer[1] > start):
+            answer = (duration, start, title)
+            
+    if answer:
+        return answer[-1]
+
+    return "(None)"
