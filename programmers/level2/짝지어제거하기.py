6054e564a4a07143a50decf903bd8036417c111d
Booooms <1222kbj@naver.com>
2022-04-15 21:12:58 +0900

    add PS

---
+++ programmers/level2/짝지어제거하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    arr= []
+    for i in s:
+        if len(arr) == 0:
+            arr.append(i)
+        elif arr[-1]==i:
+            arr.pop()
+        else:
+            arr.append(i)
+    if len(arr) == 0:
+        return 1   
+    return 0
