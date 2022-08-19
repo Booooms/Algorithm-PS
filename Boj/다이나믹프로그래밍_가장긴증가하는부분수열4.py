97ae47aad615170666bdd6508ad35fbdffc0571d
Booooms <1222kbj@naver.com>
2022-08-19 23:40:02 +0900

    add PS

---
+++ Boj/다이나믹프로그래밍_가장긴증가하는부분수열4.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = list(map(int, input().split()))
+dp = [0 for i in range(n)]
+for i in range(n):
+    for j in range(i):
+        if a[i] > a[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(max(dp))
+
+length = max(dp)
+b = []
+for i in range(n-1, -1, -1):
+    if dp[i]==length:
+        b.append(a[i])
+        length-=1
+b.reverse()
+for i in b:
+    print(i, end=' ')
