9011438ceb359723fe8f27862d19499da7be234a
Booooms <1222kbj@naver.com>
2022-10-21 23:01:35 +0900

    update PS

--- Boj/브루트포스_N과M(5).py
+++ Boj/브루트포스_N과M(5).py
@@ -9,6 +9,7 @@ def solve(depth, N, M):
     if depth == M:
         print(' '.join(map(str, out)))
         return
+        
     for i in range(N):
         if not visited[i]:
             visited[i] = True
