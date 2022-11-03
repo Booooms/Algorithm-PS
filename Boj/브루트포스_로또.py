88ca4508a25a0801e6b4b44a2411eb980c5b6da3
Booooms <1222kbj@naver.com>
2022-11-03 09:57:08 +0900

    update PS

--- Boj/브루트포스_로또.py
+++ Boj/브루트포스_로또.py
@@ -26,8 +26,10 @@ while True:
     s = list(map(int, input().split()))
     if s[0] == 0:
         break
+
     del s[0]
     s = list(combinations(s, 6))
+    
     for i in s:
         for j in i:
             print(j, end=' ')
