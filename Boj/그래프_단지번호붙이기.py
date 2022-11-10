05210c34c1d3b8930d1cdd75ac28b87d163919c9
Booooms <1222kbj@naver.com>
2022-11-10 09:55:37 +0900

    update PS

--- Boj/그래프_단지번호붙이기.py
+++ Boj/그래프_단지번호붙이기.py
@@ -4,7 +4,6 @@ from collections import deque
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-
 def bfs(graph, a, b):
     n = len(graph)
     queue = deque()
@@ -53,7 +52,6 @@ for i in range(n):
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-
 def DFS(x, y):
     if x < 0 or x >= n or y < 0 or y >= n:
         return False
@@ -68,7 +66,6 @@ def DFS(x, y):
             DFS(nx, ny)
         return True
     return False
-
 
 count = 0
 result = 0
